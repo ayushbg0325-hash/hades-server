8d5caf1c4d025574782bf580921d2b9c16885c2b
Ayush <ayush.bg0325@gmail.com>
2026-03-19 14:34:44 +0700

    fix db connection

--- server.js
+++ server.js
@@ -13,12 +13,9 @@ app.get("/", (req, res) => {
 });
 
 // MySQL connection
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+const mysql = require("mysql2");
+
+const db = mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
 
 db.connect((err) => {
   if (err) console.error("DB холболт алдаа:", err);
